978b93ef7eb87b0e4e330a85d8b61e5a399254f1
JorgeGarcia2 <garciav.jorge96@gmail.com>
2020-12-01 15:26:07 -0600

    FreqDiv with parameter

--- Sources/FreqDiv.v
+++ Sources/FreqDiv.v
@@ -1,14 +1,17 @@
-module FreqDiv (Clk_in, Clk_out);
+module FreqDiv #(parameter FREQ = 4)(Clk_in, Clk_out);
   input Clk_in;
   output reg Clk_out;
 
-  reg [19:0] cont50Hz = 20'h0;
+  reg [19:0] contHz = 0;
+  initial begin
+    Clk_out = 0;
+  end
 
   always@(posedge Clk_in) begin
-		if(cont50Hz == 0)begin
-			cont50Hz = 20'hF4240;
+		if(contHz == 0)begin
+			contHz = FREQ;
 			Clk_out = !Clk_out;
         end
-        else cont50Hz = cont50Hz - 1'h1;
+        else contHz = contHz - 1'h1;
     end
 endmodule
